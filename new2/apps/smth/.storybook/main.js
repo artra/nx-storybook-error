e148c271fa7bdd756cde7f45cfa6c4865b160205
Alexey Artemev <alexey.artemev@shell.com>
2022-09-26 17:31:09 +0200

    still broken

--- new2/apps/smth/.storybook/main.js
+++ new2/apps/smth/.storybook/main.js
@@ -4,7 +4,7 @@ const config = {
   ...rootMain,
   core: { builder: 'webpack5' },
   stories: [...rootMain.stories, '../**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-  addons: [...(rootMain.addons ?? []), '@nrwl/react/plugins/storybook'],
+  addons: [...(rootMain.addons ?? [])],
   staticDirs: ['../public', '../../../libs/design-system/public'],
   webpackFinal: async (config, options) => {
     // apply any global webpack configs that might have been specified in .storybook/main.js
