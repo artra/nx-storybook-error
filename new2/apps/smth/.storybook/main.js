0a0d5d27e10fc4d4b3f85090273551eab3f222ed
Alexey Artemev <alexey.artemev@shell.com>
2022-09-27 09:38:49 +0200

    fixed

--- new2/apps/smth/.storybook/main.js
+++ new2/apps/smth/.storybook/main.js
@@ -1,10 +1,18 @@
 const rootMain = require('../../../.storybook/main');
+const path = require('path');
 
 const config = {
   ...rootMain,
   core: { builder: 'webpack5' },
   stories: [...rootMain.stories, '../**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-  addons: [...(rootMain.addons ?? [])],
+  addons: [...(rootMain.addons ?? []),
+    'storybook-addon-swc',
+    {
+      name: 'storybook-addon-next',
+      options: {
+        nextConfigPath: path.resolve(__dirname, '../next.config.js'),
+      },
+    }],
   staticDirs: ['../public', '../../../libs/design-system/public'],
   webpackFinal: async (config, options) => {
     // apply any global webpack configs that might have been specified in .storybook/main.js
